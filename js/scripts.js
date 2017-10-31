9a3193f1e2bb94f729a54d61c04b4d7c69a9be98
Chris Knight Johnson and Ginger Lee Kretschmer <chris@>
2017-10-31 15:31:42 -0700

    add frontend and backend logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-function BankAccount(name, initialDeposit){
-  this.name = name;
+function BankAccount(user, initialDeposit){
+  this.user = user;
   this.balance = initialDeposit;
 };
 BankAccount.prototype.deposit = function (depositAmt) {
   this.balance += depositAmt;
 };
+BankAccount.prototype.withdraw = function (withdrawAmt) {
+  this.balance -= withdrawAmt;
+};
+
+
+
+// FRONTEND
+$(document).ready(function() {
+  var accounts = [];
+  $("#register-form").submit(function(event) {
+    var userName = $('#input-new-user').val();
+    var initialDeposit = $('#input-initial-deposit').val();
+    var newUser = new BankAccount(userName, initialDeposit);
+    event.preventDefault();
+  });
+
+});
